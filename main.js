90bf1d61e5435ce476151683c8db9509eeb0886a
seriksk1 <55358000+seriksk1@users.noreply.github.com>
2021-07-03 11:16:45 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -32,29 +32,29 @@ let city = {
       });
   },
   showWeather(data) {
-    document.querySelector("#city").style.display = "flex"
     document.querySelector(".city-name h2").innerText = data.name
     document.querySelector(".city-weather-state").innerText = data.weather[0].main
     document.querySelector(".city-humidity span").innerText = data.main.humidity
     document.querySelector(".city-temperature span").innerText = Math.round(data.main.temp)
     document.querySelector(".weather-icon").src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
     document.querySelector(".weather-icon").title = data.weather[0].main
+    document.querySelector(".city").style.display = "flex";
   },
 };
 
-window.addEventListener("load", function () {
+window.addEventListener("load", () => {
   if (localStorage.getItem("userCity") !== null) {
     city.search(localStorage.getItem("userCity"))
   }
 });
 
 const favorite = document.querySelector(".city-name img");
-favorite.addEventListener("click", function () {
-    localStorage.setItem("userCity", sessionStorage.getItem("currentCity"))
-    city.toggleFavorite();
+favorite.addEventListener("click", () => {
+  localStorage.setItem("userCity", sessionStorage.getItem("currentCity"));
+  city.toggleFavorite();
 });
 
 document.querySelector("#search-btn")
-.addEventListener("click", function () {
+.addEventListener("click", () => {
   city.search(document.querySelector("#searchCity").value);
 });
