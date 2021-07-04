b876d47d5d5dc4d95539d158692f71df88769846
seriksk1 <55358000+seriksk1@users.noreply.github.com>
2021-07-04 16:31:40 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,22 @@
 let city = {
   API_KEY: "ff9d437b8a5c1fdf6bc59832839e29b8",
+  current: "",
+
+  isFavorite(currentCity) {
+    return localStorage.getItem("userCity") === currentCity;
+  },
+
   toggleFavorite() {
-    if ((localStorage.getItem("userCity") !== null) && favorite.src.includes("not-filled")) {
+    if (localStorage.getItem("userCity") !== null && favorite.src.includes("not-filled")) {
       favorite.src = "img/black-heart.svg";
     } else {
       favorite.src = "img/black-heart-not-filled.svg";
-      if (localStorage.getItem("userCity") === sessionStorage.getItem("currentCity")) {
-          localStorage.removeItem("userCity");  
+      if (this.isFavorite(this.current)) {
+        localStorage.removeItem("userCity");
       }
     }
   },
+
   search(name) {
     fetch(
       `https://api.openweathermap.org/data/2.5/weather?q=${name}&units=metric&appid=${this.API_KEY}`
@@ -18,27 +25,30 @@ let city = {
         if (res.ok) {
           return res.json();
         } else {
-          alert("Wrong city");
+          document.querySelector(".city").style.display = "none";
+          document.querySelector(".error").style.display = "block";
           throw Error("Wrong city");
         }
       })
       .then((data) => {
-        sessionStorage.setItem("currentCity", data.name)
-        this.toggleFavorite()
-        this.showWeather(data)
+        this.current = data.name;
+        this.toggleFavorite();
+        this.showWeather(data);
       })
       .catch((error) => {
-        console.log(error)
+        console.log(error);
       });
   },
+
   showWeather(data) {
-    document.querySelector(".city-name h2").innerText = data.name
-    document.querySelector(".city-weather-state").innerText = data.weather[0].main
-    document.querySelector(".city-humidity span").innerText = data.main.humidity
-    document.querySelector(".city-temperature span").innerText = Math.round(data.main.temp)
-    document.querySelector(".weather-icon").src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`
-    document.querySelector(".weather-icon").title = data.weather[0].main
+    document.querySelector(".city-name h2").innerText = data.name;
+    document.querySelector(".city-weather-state").innerText = data.weather[0].main;
+    document.querySelector(".city-humidity span").innerText = data.main.humidity;
+    document.querySelector(".city-temperature span").innerText = Math.round(data.main.temp);
+    document.querySelector(".weather-icon").src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+    document.querySelector(".weather-icon").title = data.weather[0].main;
     document.querySelector(".city").style.display = "flex";
+    document.querySelector(".error").style.display = "none";
   },
 };
 
@@ -50,7 +60,7 @@ window.addEventListener("load", () => {
 
 const favorite = document.querySelector(".city-name img");
 favorite.addEventListener("click", () => {
-  localStorage.setItem("userCity", sessionStorage.getItem("currentCity"));
+  localStorage.setItem("userCity", city.current);
   city.toggleFavorite();
 });
 
